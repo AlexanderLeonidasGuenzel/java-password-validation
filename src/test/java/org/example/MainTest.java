b1e5c31ddace9941813c8473be7da1bfdce42db5
Enrico <e.weissenrieder94@gmail.com>
2023-06-02 11:05:43 +0200

    test password length check

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+@Test
+void checkPasswordLength8(
+){
+    Assertions.assertTrue(Main.checkPasswordLength());
+}
 
+    @Test
+    void checkPasswordLengthNot8(
+    ){
+        Assertions.assertFalse(Main.checkPasswordLength());
+    }
 
 }
