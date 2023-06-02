33262f6b189bdfc14194ceb0816b6c4a25875335
Enrico <e.weissenrieder94@gmail.com>
2023-06-02 11:52:26 +0200

    merged main into test branch

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,19 +2,34 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-@Test
-void checkPasswordLength8(
-){
-    Assertions.assertTrue(Main.checkPasswordLength());
-}
+
+ @Test
+    void containNumbersvalidatePasswordTrue(){
+     String password = "passwort1";
+
+     assertTrue(Main.validatePassword(password));
+
+ }
+
+ @Test
+    void containNumbersvalidatePasswordFalse(){
+     String password = "passwort";
+
+     assertFalse(Main.validatePassword(password));
+ }
+    @Test
+    void checkPasswordLength8(
+    ){
+        Assertions.assertTrue(Main.checkPasswordLength());
+    }
 
     @Test
     void checkPasswordLengthNot8(
     ){
         Assertions.assertFalse(Main.checkPasswordLength());
     }
-
 }
